ea9f8bdddf409cf1c59f0ec586b86995294e1d53
Isabelle <asahifoo@Asahi-Foos-MacBook-Pro-2.local>
2017-02-06 10:38:26 +1100

    tidy up data

--- 01_tidy_data.R
+++ 01_tidy_data.R
@@ -4,4 +4,11 @@ library(tidyverse)
 bat_dat <- readr::read_csv("./data/bat_dat.csv", 
                            col_types = cols(Season = col_factor(levels = c(1,2)), Habitat = col_factor(levels = c(0:1)), Bioregion = col_factor (levels = c(1:4, NA)))
                            )
-iris_df <- readr::read_csv("./data/iris.csv")
+read_csv("./data/iris.csv", comment = "#", 
+         col_types = cols(
+           Sepal.Length = col_double(),
+           Sepal.Width = col_double(),
+           Petal.Length = col_double(),
+           Petal.Width = col_double(),
+           Species = col_factor(c("setosa", "versicolor", "virginica"))
+         ))

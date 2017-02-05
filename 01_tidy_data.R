a972a66d4914f51991e6f8ac2f2dcc1da7d3b490
Isabelle <asahifoo@Asahi-Foos-MacBook-Pro-2.local>
2017-02-06 10:21:18 +1100

    tidied up columns of bat_dat

--- 01_tidy_data.R
+++ 01_tidy_data.R
@@ -1,3 +1,7 @@
 #loads all the tidyverse stuff
 
 library(tidyverse)
+bat_dat <- readr::read_csv("./data/bat_dat.csv", 
+                           col_types = cols(Season = col_factor(levels = c(1,2)), Habitat = col_factor(levels = c(0:1)), Bioregion = col_factor (levels = c(1:4, NA)))
+                           )
+iris_df <- readr::read_csv("./data/iris.csv")

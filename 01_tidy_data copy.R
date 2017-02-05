f370f4bcebfc6e0678dd3c7447b8167736c89ebe
Isabelle <asahifoo@Asahi-Foos-MacBook-Pro-2.local>
2017-02-06 10:49:18 +1100

    copied idk why didnt work

---
+++ 01_tidy_data copy.R
@@ -0,0 +1,14 @@
+#loads all the tidyverse stuff
+
+library(tidyverse)
+bat_dat_df <- readr::read_csv("./data/bat_dat.csv", 
+                           col_types = cols(Season = col_factor(levels = c(1,2)), Habitat = col_factor(levels = c(0:1)), Bioregion = col_factor (levels = c(1:4, NA)))
+                           )
+iris_df <- read_csv("./data/iris.csv", comment = "#", 
+         col_types = cols(
+           Sepal.Length = col_double(),
+           Sepal.Width = col_double(),
+           Petal.Length = col_double(),
+           Petal.Width = col_double(),
+           Species = col_factor(c("setosa", "versicolor", "virginica"))
+         ))

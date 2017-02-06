2723b10b34326b9d4bcdb4d51497162d9b56dd84
Isabelle <asahifoo@Asahi-Foos-MacBook-Pro-2.local>
2017-02-06 11:04:41 +1100

    changed df name

--- 01_tidy_data.R
+++ 01_tidy_data.R
@@ -1,10 +1,10 @@
 #loads all the tidyverse stuff
 
 library(tidyverse)
-bat_dat_df <- readr::read_csv("./data/bat_dat.csv", 
+bat_dat <- readr::read_csv("./data/bat_dat.csv", 
                            col_types = cols(Season = col_factor(levels = c(1,2)), Habitat = col_factor(levels = c(0:1)), Bioregion = col_factor (levels = c(1:4, NA)))
                            )
-iris_df <- read_csv("./data/iris.csv", comment = "#", 
+iris <- read_csv("./data/iris.csv", comment = "#", 
          col_types = cols(
            Sepal.Length = col_double(),
            Sepal.Width = col_double(),
